16e3fb20600837b89c7fa085ca9f4fc5682f6d87
rook91 <rook01093@gmail.com>
2021-05-02 11:51:19 +0200

    home and away stats

--- src/App.js
+++ src/App.js
@@ -4,55 +4,22 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import sportsmonksPolandFixtures from './data/sportmonks/scottish2020-2021Fixtures.json'
 
+const getRow = (index) => {
+  return(
+    <div key={sportsmonksPolandFixtures.data[index].id}>
+      <span>ID: {sportsmonksPolandFixtures.data[index].id}</span>
+      <span> Home Team ID: {sportsmonksPolandFixtures.data[index].localteam_id}</span>
+      <span> Away Team ID: {sportsmonksPolandFixtures.data[index].visitorteam_id}</span>
+    </div>
+  )
+}
+
 function App() {
-  console.log(sportsmonksPolandFixtures);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      {sportsmonksPolandFixtures.data.map((item, index) => getRow(index))}
+      <Counter />
     </div>
   );
 }
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -9,6 +9,13 @@ import {
   selectCount,
 } from './counterSlice';
 import styles from './Counter.module.css';
+import sportsmonksPolandFixtures from '../../data/sportmonks/scottish2020-2021Fixtures.json'
+
+const getFixtureDate = fixture => new Date(fixture.time.starting_at.date_time);
+const getFixture = (id) => sportsmonksPolandFixtures.data.find(fixture => fixture.id === id);
+const getTeamMatches = (id) => sportsmonksPolandFixtures.data.filter(fixture => (fixture.localteam_id === id || fixture.visitorteam_id === id))
+const compareFixturesDate = (firstFixture, secondFixture) => (getFixtureDate(firstFixture) < getFixtureDate(secondFixture)) ? 1 : -1;
+const isOver = scores => scores.localteam_score + scores.visitorteam_score > 2.5;
 
 export function Counter() {
   const count = useSelector(selectCount);
@@ -20,46 +27,53 @@ export function Counter() {
   return (
     <div>
       <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
         <input
           className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
+          onChange={(e) => setIncrementAmount(Number(e.target.value))}
         />
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
+          onClick={() => {
+            const stats = {
+              h2hlast6: {
+                over: 0,
+                under: 0
+              },
+              h2hlast3Home: {
+                over: 0,
+                under: 0
+              },
+              home: {
+                over: 0,
+                under: 0
+              },
+              away: {
+                over: 0,
+                under: 0
+              }
+            };
+            const fixture = getFixture(incrementAmount);
+            const homeTeam = fixture.localteam_id;
+            const awayTeam = fixture.visitorteam_id;
+            const matchDate = getFixtureDate(fixture);
+            const homeTeamMatches = getTeamMatches(homeTeam);
+            const homeTeamMatchesSorted = homeTeamMatches.sort(compareFixturesDate);
+            const homeTeamMatchesFiltered = homeTeamMatchesSorted.filter(match => getFixtureDate(match) < matchDate).slice(0, 10);
+            const awayTeamMatches = getTeamMatches(awayTeam);
+            const awayTeamMatchesSorted = awayTeamMatches.sort(compareFixturesDate);
+            const awayTeamMatchesFiltered = awayTeamMatchesSorted.filter(match => getFixtureDate(match) < matchDate).slice(0, 10);
+
+            homeTeamMatchesFiltered.forEach(i => {
+              isOver(i.scores) ? stats.home.over++ : stats.home.under++
+              console.log(`${i.id} ${i.scores.localteam_score}:${i.scores.visitorteam_score} is over ****${isOver(i.scores)}****`);
+            });
+            awayTeamMatchesFiltered.forEach(i => isOver(i.scores) ? stats.away.over++ : stats.away.under++);
+            console.log(stats);
+          }}
         >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
+          Check Fixture
         </button>
       </div>
     </div>
